474f187a65b61337bb842a08d4f8f3f5fdd226f1
Thalia <61909523+Thalia-J@users.noreply.github.com>
2026-03-17 17:57:08 -0400

    Update main.py
    Added more intro work, got up to frame analysis and function generation

--- main.py
+++ main.py
@@ -4,23 +4,14 @@ import cv2
 from scipy.optimize import curve_fit
 
 def gen_linear_A(m,vars):
-    datapoint = range(m) #assuming we have equally placed points, thus range
+    datapoint = range(1,m+1) #assuming we have equally placed points, thus range
     A = np.zeros((m,vars))
-    for i in datapoint:
+    for i in range(datapoint):
         for j in range(vars):
             A[i][j] = datapoint[i]**j
     return A
 
-def gen_kern(m, c):
-    A = np.ones((m,m))
-    if c:
-        for i in range(m):
-            for j in range(m):
-                A[i][j] = np.exp(-((i-m)**2)/2) * np.exp(-((j-m)**2)/2)
-    else:
-        A = A / (m**2)
-    
-    return A
+
 
 
 
@@ -34,9 +25,11 @@ def fun1(x, a, b, c, d):
 
 video_seq = []
 
-file_name = input("What .mp4 file should be used?")
+file_name = input("What .mp4 file should be used? \n")
 video_info = cv2.VideoCapture(file_name)
 fps = video_info.get(cv2.CAP_PROP_FPS)
+
+#putting the data into a array
 while video_info.isOpened():
     ret, frame = video_info.read()
     
@@ -45,17 +38,47 @@ while video_info.isOpened():
 
     video_seq.append(frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    
 
-data_amount = input("How many frames should be used for interpolation")
-convolution = input("what is the odd N, NxN convolution applied to frames")
-scaling = input("Should there be euler dropoff for the muddling(1/0)")
+data_amount = input("How many frames should be used for interpolation\n")
+polyamnt = input("what number polynomial approximation should be used? 0 if using custom\n")
+convolution = input("what is the odd N, NxN convolution applied to frames\n")
+scaling = input("Should there be euler dropoff for the muddling(1/0)\n")
 
-kern = gen_kern(convolution, scaling)
+Amat = []
+if polyamnt != 0:
+    Amat = gen_linear_A(data_amount,polyamnt)
+
+solution_vec = np.array(np.zeros(len(video_seq[0]),len(video_seq[0,0])), dtype=list)
+#numpy array in the shape of [x,y,color] which stores lists where we will deposit the x's for the function solutions
+#having different functions for the colors is... suboptimal
+
+for i in range(len(video_seq)/data_amount):
+    interdata = video_seq[i:(i+data_amount)] #gets a subsection of the frames
+    for i in range(len(interdata)):
+        if scaling==0:
+            interdata[i] = cv2.cvtColor(interdata[i], cv2.COLOR_BGR2GRAY)
+            interdata[i] = cv2.blur(interdata[i], (convolution,convolution))
+            
+        else:
+            interdata[i] = cv2.cvtColor(interdata[i], cv2.COLOR_BGR2GRAY)
+            interdata[i] = cv2.GaussianBlur(interdata[i], (convolution,convolution), 0)
+            
+    
+    #interdata[frame, x, y, color]
+    if Amat != []:
+        #we use scipy.optimize.nnls for non negative least squares approximation for each color
+        ymat = interdata(:,x,y,0)
+    else:
+        #this is using scipy.optimize.least_squares for nonlinear least squares
+
+    
+    
 
 
 
 
+
+#freeing video after capturing it
 video_info.release()
 
